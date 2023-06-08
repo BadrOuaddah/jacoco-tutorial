0a7702e621f78096e583b76a8fbb5dcac1373f5d
Admin <Badr.Ouaddah.07@gmail.com>
2023-06-08 14:41:47 +0100

    Create simple example with gradle and unit testing

---
+++ src/test/java/com/tutorial/jacoco/UserTest.java
@@ -0,0 +1,19 @@
+package com.tutorial.jacoco;
+
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+public class UserTest {
+    User user = new User();
+
+    @Test
+    void testUserWithName() {
+        Assert.assertEquals("Username : Davie is added successfully",user.addUser("Davie"));
+    }
+    @Test
+    void testUserWithNull() {
+        Assert.assertEquals("Please provide the username",user.addUser(null));
+    }
+}
