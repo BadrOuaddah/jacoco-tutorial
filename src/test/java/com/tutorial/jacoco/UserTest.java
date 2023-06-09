2ed9a94f43971d0a3b2ffc26467e719ca5b3f383
Admin <Badr.Ouaddah.07@gmail.com>
2023-06-09 21:07:19 +0100

    Fix tests not running by change the methods visibility to public

--- src/test/java/com/tutorial/jacoco/UserTest.java
+++ src/test/java/com/tutorial/jacoco/UserTest.java
@@ -1,19 +1,21 @@
 package com.tutorial.jacoco;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 
-class UserTest {
+
+public class UserTest {
 
     User user = new User();
 
     @Test
-    void testUserWithName() {
+    public void testUserWithName() {
         Assert.assertEquals("Username : Davie is added successfully",user.addUser("Davie"));
     }
     @Test
-    void testUserWithNull() {
+    public void testUserWithNull() {
         Assert.assertEquals("Please provide the username",user.addUser(null));
     }
+
 }
